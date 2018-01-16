d90377bbcfa29195758e1636930c183e2ba41f23
Nikita Yovchev <31662427+nikitayvchv@users.noreply.github.com>
2018-01-16 09:56:11 -0500

    Create What's for Dinner.js

---
+++ Intro To Java/What's for Dinner.js
@@ -0,0 +1,33 @@
+background(186, 145, 20); // wooden table
+ellipse(200, 200, 350, 350); // plate
+ellipse(200, 200, 300, 300); 
+
+//Bun of hotdog
+fill(191, 161, 27);
+rect(145,111,107,197,65);
+
+//Hotdog
+fill (171, 120, 25);
+rect(166,105,66,208,126);
+
+//Fries
+noStroke();
+fill(255, 230, 0);
+rect(84,117,10,99);
+rect(97,131,10,99);
+rect(110,117,10,99);
+rect(125,98,10,99);
+rect(125,200,10,99);
+rect(113,212,10,99);
+rect(92,181,10,99);
+
+//Ketchup
+fill(255, 0, 0);
+ellipse(286,138,35,35);
+ellipse(280,119,35,35);
+ellipse(303,134,35,35);
+
+//Mustard
+strokeWeight(20);
+stroke(245, 204, 0);
+line(197,140,201,275);
